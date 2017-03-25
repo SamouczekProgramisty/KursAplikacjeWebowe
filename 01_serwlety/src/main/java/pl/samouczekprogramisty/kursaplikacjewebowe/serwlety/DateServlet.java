b9a0a61fd29aec81d082a4a43d9bb5e66e69172b
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-03-25 11:44:07 +0100

    Initial commit, first simple web application

---
+++ 01_serwlety/src/main/java/pl/samouczekprogramisty/kursaplikacjewebowe/serwlety/DateServlet.java
@@ -0,0 +1,22 @@
+package pl.samouczekprogramisty.kursaplikacjewebowe.serwlety;
+
+
+import org.joda.time.DateTime;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/date")
+public class DateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        DateTime now = DateTime.now();
+        PrintWriter responseOutput = response.getWriter();
+        responseOutput.append("<html><body>" + now.toString() + "</body></html>");
+    }
+}
